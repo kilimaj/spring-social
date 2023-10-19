8aa2900b956dbe4dd72195b45057f54f6c0543f7
John Kilima <jkbundala@gmail.com>
2023-10-19 06:04:33 +0300

    Initial setup of the backend

---
+++ springsocial-backend/src/main/java/dev/kilima/springsocial/SpringsocialBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.kilima.springsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsocialBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsocialBackendApplication.class, args);
+	}
+
+}
---
+++ springsocial-backend/src/test/java/dev/kilima/springsocial/SpringsocialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kilima.springsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsocialBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
